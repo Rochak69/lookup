39a3d110215c2abe3d0fe7e07af7447790a75316
Rochak-ES <rochak@esignature.com.np>
2023-12-19 16:11:09 +0545

    fix: move app entry point

--- lib/build_variants/environment_entry_points.dart
+++ lib/build_variants/environment_entry_points.dart
@@ -1,4 +1,4 @@
-import 'package:lookup/build_variants/app_entry_point.dart';
+import 'package:lookup/app_entry_point.dart';
 
 class DevelopmentEntryPoint extends AppEntryPoint {
   DevelopmentEntryPoint(super.appConfig);
