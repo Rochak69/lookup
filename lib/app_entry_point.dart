7a976e334b9bab740941804e45c7f1b20900f222
RochakShrestha19 <rochak.shrestha@varosatech.com>
2023-12-19 20:24:18 +0545

    feature: 120 hz refresh rate

--- lib/app_entry_point.dart
+++ lib/app_entry_point.dart
@@ -1,8 +1,9 @@
+import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_displaymode/flutter_displaymode.dart';
 import 'package:lookup/core/config/app_bloc.dart';
-
 import 'package:lookup/core/config/app_config.dart';
 import 'package:lookup/core/hive/hive_manager.dart';
 import 'package:lookup/core/injection/dependency_injection.dart';
@@ -28,13 +29,22 @@ class AppEntryPoint {
         error: details,
       );
     };
-
+    unawaited(setHightRefreshRate());
     Bloc.observer = const AppBlocObserver();
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
     await configureDependencies();
     await sl<HiveManager>().init();
+
     runApp(App());
+  }
+
+  Future<void> setHightRefreshRate() async {
+    try {
+      await FlutterDisplayMode.setHighRefreshRate();
+    } catch (e) {
+      PrettyLogger.logger.e(e.toString());
+    }
   }
 }
